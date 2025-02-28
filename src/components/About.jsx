65c0ddc50411bd90bbdb4506c39a58f3bde17501
Akila <akilaewis@gmail.com>
2025-02-28 19:35:58 +0530

    Add Navigation Bar and React Router for multiple pages

---
+++ src/components/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className='about'>
+        <h2>About Me</h2>
+        <p>Hi, I'm Jhon Doe, a software developer with a passion for building software solutions</p>
+    </div>
+  );
+};
+
+export default About;

e71552e710df0a653d433636d5eb5d434a6f4379
Akila <akilaewis@gmail.com>
2025-03-05 15:59:54 +0530

    Added the projects page and with the styles

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import About from './components/About';
+import Projects from './components/Projects';
 import './App.css'
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Home/>}/>
           <Route path='/about' element={<About/>}/>
+          <Route path='/projects' element={<Projects/>}/>
         </Routes>
       </div>
       </Router>
---
+++ src/components/Projects.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Projects.css';
+
+function Projects() {
+  return (
+    <div className='project-container'>
+        <div className="project-list">
+        <h2>My Projects</h2>
+            <div className="project-card">Project 01</div>
+            <div className="project-card">Project 02</div>
+            <div className="project-card">Project 03</div>
+        </div>
+    </div>
+  )
+}
+
+export default Projects
